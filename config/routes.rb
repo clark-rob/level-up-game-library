390da416a37e496f0a4b88d4b8cb256de1083a2f
clark-rob <clark.rob.g@gmail.com>
2018-11-28 09:23:13 -0500

    Routes modified
    - added users resource in routes
      - only able to handle show/find & update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users, only: %i[index show update]
   resources :games, except: %i[new edit]
   resources :consoles, except: %i[new edit]
   # RESTful routes
